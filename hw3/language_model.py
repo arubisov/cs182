4b0f105c3342163b80d279b584a970ed42ff3128
Anton Rubisov <anton.rubisov@gmail.com>
2023-08-28 13:17:28 -0400

    implement lstm language model

--- hw3/language_model.py
+++ hw3/language_model.py
@@ -10,10 +10,10 @@ class LanguageModel(nn.Module):
 
         self.V = vocab_size
         self.H = rnn_size
-        
+
         # Create an embedding layer of shape [vocab_size, rnn_size]
         # That will map each word in our vocab into a vector of rnn_size size.
-        self.embedding = nn.Embedding(num_embeddings=self.V, 
+        self.embedding = nn.Embedding(num_embeddings=self.V,
                                       embedding_dim=self.H)
 
         # Create an LSTM layer of rnn_size size. Use any features you wish.
@@ -25,19 +25,20 @@ class LanguageModel(nn.Module):
                             batch_first=True,       # input and output tensors are provided as (batch, seq, feature) instead of (seq, batch, feature)
                             dropout=dropout,        # dropout layer after each LSTM layer (except last layer) with p=dropout
                             bidirectional=False)    # if True, becomes a bidirectional LSTM
-        
+
         # LSTM layer does not add dropout to the last hidden output.
         # Add this if you wish.
         self.dropout = nn.Dropout(p=dropout)
-        
+
         # Use a dense layer to project the outputs of the RNN cell into logits of
         # the size of vocabulary (vocab_size).
         self.output = nn.Linear(in_features=self.H,
                                 out_features=self.V)
-        
+
+
     def forward(self, x):
         """Run a forward pass through the Language Model and return raw scores (logits).
-        
+
         Args:
         - x (torch.Tensor): minibatch of input strings, dim (B, T) where
                             B = minibatch size
@@ -49,25 +50,26 @@ class LanguageModel(nn.Module):
         """
 
         B, T = x.shape
-        
-        # project the each input in minibatch into an embedding of hidden_size. 
+
+        # project the each input in minibatch into an embedding of hidden_size.
         # returns size (B, T, H)
         embeds = self.embedding(x)
-        
+
         # pass embedding through LSTM
         # returns size (B, T, H)
         lstm_out, _ = self.lstm(embeds)
-        
+
         # pass last LSTM output through a dropout layer.
         # returns size (B, T, H)
         lstm_drop = self.dropout(lstm_out)
 
         # pass LSTM output through an affine layer mapping the hidden_size to
-        # the vocab size. 
+        # the vocab size.
         # (B, T, H) -> (B*T, H) @ (H, V) -> (B*T, V) -> (B, T, V)
         logits = self.output(lstm_drop.reshape(B*T, self.H)).reshape(B, T, self.V)
-        
+
         return logits
+
 
     def loss(self, pred, target, mask):
         """Cross-entropy loss on the predictions."""
@@ -92,3 +94,8 @@ class LanguageModel(nn.Module):
         batch_loss = (loss_masked.sum(dim=1) / (loss_masked != 0).sum(dim=1)).mean()
 
         return batch_loss
+
+
+    def check_accuracy(self, pred, target, mask):
+        """Check batch accuracy: % of non-masked tokens where predicted class (vocab item) is equal to the target class"""
+        return (torch.eq(prediction.argmax(dim=2,keepdim=False), batch_target).float()*batch_target_mask).sum() / batch_target_mask.sum()
